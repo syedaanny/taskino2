700322771dc4c007540c0270ff608c10fba503e4
Syeda Anny Akther <syedaanny02@gmail.com>
2023-09-02 23:01:18 +0600

    taskino2

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,22 @@ import './globals.css'
 import '@/styles/Navbar.css'
 // import '@/styles/Bigscreen.css'
 import '@/styles/HomeSection.css'
-import { Inter } from 'next/font/google'
+import { Inter, Plus_Jakarta_Sans} from 'next/font/google'
 import Head from 'next/head'
 import Link from 'next/link'
 import Script from 'next/script'
+import { Poppins } from 'next/font/google';
 
 
-const inter = Inter({ subsets: ['latin'] })
+
+const plus_Jakarta_sans = Plus_Jakarta_Sans({ subsets: ['latin'] });
+const poppins = Poppins({
+  weight: ['400', '700','600','500'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 
 export const metadata = {
   title: 'Taskino | A task management and productivity app ',
@@ -16,16 +25,20 @@ export const metadata = {
 }
 
 
+
+
 export default function RootLayout({ children }) {
   return (
 
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
    <link rel="icon" href="/favicon.png" type="image/x-icon" sizes="any"/>
 <Head>
 <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   </Head>
-
-      <body className={inter.className}>{children}</body>
+ 
+      <body className={plus_Jakarta_sans.className}>
+          {children}
+          </body>
 <Script src="https://kit.fontawesome.com/be98f66bcc.js" crossorigin="anonymous"/>
    
     </html>
