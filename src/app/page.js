fdb680bbd8e4e555d1f0c8b1adaa1fee95ec1520
Syeda Anny Akther <syedaanny02@gmail.com>
2023-09-14 17:53:30 +0600

    taskino2

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-import Navbar from '@/Components/Navbar';
 import HomeSection from '@/Components/HomeSection';
 
 const Home = () => {
   return (
     <div>
+
       <HomeSection />
 
-      <Navbar />
     
     </div>
   );
